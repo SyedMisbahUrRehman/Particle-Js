922651f04e2cd63ea63e3f8c621d4afc108c9f95
Syed Misbah Ur Rehman <syedmisbah588@gmail.com>
2022-09-29 20:32:37 +0500

    New settings
     From (https://codepen.io/matteobruni/pen/yLNvqxG)

--- src/ParticleBackground.js
+++ src/ParticleBackground.js
@@ -21,115 +21,132 @@ const App = () => {
             init={particlesInit}
             loaded={particlesLoaded}
             options={{              //yhn paste krni hn settings
-                particles: {
-                  number: {
-                    value: 175,
-                    density: {
-                      enable: false,
-                      value_area: 800
-                    }
-                  },
-                  color: {
-                    value: "#b32c73"
-                  },
-                  shape: {
-                    type: "star",
-                    stroke: {
-                      width: 0,
-                      color: "#000000"
-                    },
-                    polygon: {
-                      nb_sides: 5
-                    },
-                    image: {
-                      src: "http://wiki.lexisnexis.com/academic/images/f/fb/Itunes_podcast_icon_300.jpg",
-                      width: 100,
-                      height: 100
-                    }
-                  },
-                  opacity: {
-                    value: 0.5,
-                    random: false,
-                    anim: {
-                      enable: false,
-                      speed: 1,
-                      opacity_min: 0.1,
-                      sync: false
-                    }
-                  },
-                  size: {
-                    value: 4,
-                    random: true,
-                    anim: {
-                      enable: false,
-                      speed: 40,
-                      size_min: 0.1,
-                      sync: false
-                    }
-                  },
-                  line_linked: {
-                    enable: false,
-                    distance: 150,
-                    color: "#ffffff",
-                    opacity: 0.4,
-                    width: 1
-                  },
-                  move: {
+              fpsLimit: 60,
+              particles: {
+                number: {
+                  value: 80,
+                  density: {
                     enable: true,
-                    speed: 14,
-                    direction: "left",
-                    random: false,
-                    straight: true,
-                    out_mode: "out",
-                    bounce: false,
-                    attract: {
-                      enable: false,
-                      rotateX: 600,
-                      rotateY: 1200
-                    }
+                    value_area: 800
                   }
                 },
-                interactivity: {
-                  detect_on: "canvas",
-                  events: {
-                    onhover: {
-                      enable: false,
-                      mode: "grab"
-                    },
-                    onclick: {
-                      enable: true,
-                      mode: "repulse"
-                    },
-                    resize: true
+                color: {
+                  value: "#ffffff"
+                },
+                shape: {
+                  type: "circle",
+                  stroke: {
+                    width: 0,
+                    color: "#000000"
                   },
-                  modes: {
-                    grab: {
-                      distance: 200,
-                      line_linked: {
-                        opacity: 1
-                      }
-                    },
-                    bubble: {
-                      distance: 400,
-                      size: 40,
-                      duration: 2,
-                      opacity: 8,
-                      speed: 3
-                    },
-                    repulse: {
-                      distance: 200,
-                      duration: 0.4
-                    },
-                    push: {
-                      particles_nb: 4
-                    },
-                    remove: {
-                      particles_nb: 2
-                    }
+                  polygon: {
+                    nb_sides: 5
+                  },
+                  image: {
+                    src: "images/github.svg",
+                    width: 100,
+                    height: 100
                   }
                 },
-                retina_detect: true
-              }}
+                opacity: {
+                  value: 1,
+                  random: false,
+                  anim: {
+                    enable: false,
+                    speed: 1,
+                    opacity_min: 0.1,
+                    sync: false
+                  }
+                },
+                size: {
+                  value: 30,
+                  random: true,
+                  anim: {
+                    enable: false,
+                    speed: 40,
+                    size_min: 0.1,
+                    sync: false
+                  }
+                },
+                line_linked: {
+                  enable: true,
+                  distance: 150,
+                  color: "#ffffff",
+                  opacity: 1,
+                  width: 1
+                },
+                move: {
+                  enable: true,
+                  speed: 2,
+                  direction: "none",
+                  random: false,
+                  straight: false,
+                  out_mode: "out",
+                  attract: {
+                    enable: false,
+                    rotateX: 600,
+                    rotateY: 1200
+                  }
+                }
+              },
+              interactivity: {
+                detect_on: "canvas",
+                events: {
+                  onHover: {
+                    enable: true,
+                    mode: "bubble",
+                    parallax: {
+                      enable: false,
+                      force: 60,
+                      smooth: 10
+                    }
+                  },
+                  onClick: {
+                    enable: true,
+                    mode: "push"
+                  },
+                  resize: true
+                },
+                modes: {
+                  grab: {
+                    distance: 400,
+                    lineLinked: {
+                      opacity: 1
+                    }
+                  },
+                  bubble: {
+                    distance: 400,
+                    size: 100,
+                    duration: 2,
+                    opacity: 1,
+                    speed: 3
+                  },
+                  repulse: {
+                    distance: 200
+                  },
+                  push: {
+                    particles_nb: 4
+                  },
+                  remove: {
+                    particles_nb: 2
+                  }
+                }
+              },
+              backgroundMask: {
+                enable: true,
+                cover: {
+                  value: {
+                    r: 255,
+                    g: 255,
+                    b: 255
+                  }
+                }
+              },
+              retina_detect: true,
+              background: {
+                image: "url('https://particles.js.org/images/background.jpg')",
+                position: "cover"
+              }}}
         />
     );
 };
